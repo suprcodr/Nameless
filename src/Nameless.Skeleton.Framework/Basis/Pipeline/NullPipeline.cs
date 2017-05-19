2c1a82d09c13a712112c4078dec770586d9dbe63
marcoaoteixeira <marcoaoteixeira@gmail.com>
2017-05-19 11:48:49 -0300

    changes...all

---
+++ src/Nameless.Skeleton.Framework/Basis/Pipeline/NullPipeline.cs
@@ -0,0 +1,35 @@
+﻿namespace Nameless.Skeleton.Framework.Basis.Pipeline {
+
+    /// <summary>
+    /// Null Object Pattern implementation for <see cref="IPipeline{T}"/>.
+    /// </summary>
+    /// <remarks>https://en.wikipedia.org/wiki/Null_Object_pattern</remarks>
+    public sealed class NullPipeline<T> : IPipeline<T> {
+
+        #region Public Static Fields
+
+        /// <summary>
+        /// Gets the static current instance of <see cref="NullPipeline{T}"/>.
+        /// </summary>
+        public static readonly IPipeline<T> Instance = new NullPipeline<T>();
+
+        #endregion Public Static Fields
+
+        #region Private Constructors
+
+        // Block construction of NullLogger
+        private NullPipeline() { }
+
+        #endregion Private Constructors
+
+        #region IPipeline<T> Members
+
+        /// <inheritdoc />
+        public void Execute(T input) { }
+
+        /// <inheritdoc />
+        public IPipeline<T> RegisterFilter(IFilter<T> filter) { return this; }
+
+        #endregion IPipeline<T> Members
+    }
+}
